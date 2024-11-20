c7b981d87882b3c65e6a8b9ec4c20904dc45f5f2
savegod81 <26392985@qq.com>
2024-11-20 17:28:51 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import streamlit as st
-message = []
 if 'key' not in st.session_state:
-    st.session_state['key'] = message
-	
+    st.session_state['key'] = []
 
 st.title("Our Private Chatroom")
-input = st.text_input("Your Message1:")
+input = st.text_input("Your Message2:")
 
 all_msg = st.session_state.key
+all_msg.append(input)
+st.session_state['key'] = message
 for msg in all_msg:
 	st.write(msg)
