8f61e843144b0cfe2e409b4d67a19179282bb5a3
savegod81 <savegod@sohu.com>
2024-11-20 19:20:20 +0800

    update

--- main.py
+++ main.py
@@ -5,15 +5,29 @@ if 'key' not in st.session_state:
 messages = []
 
 @st.cache_data
+def get_conn():
+	conn = st.connection('messages_db', type='sql')
+	return conn
+
+conn = get_conn()
+
 def save(msg):
-	messages.append(msg)
-	return messages
+	with conn.session as s:
+    	s.execute('CREATE TABLE IF NOT EXISTS chat_messages(msg_ts timestamp, message TEXT);')
+    	s.execute(
+    		'INSERT INTO chat_messages (msg_ts, message) VALUES (current_timestamp, :msg);',
+            params=dict(msg=msg)
+    	)
+    	s.commit()
+
+def getAll():
+	sql = 'select * from chat_messages order by msg_ts desc'
+	results = conn.query(sql=sql, ttl=10)
+	st.dataframe(results)
 
 st.title("Our Private Chatroom")
 input = st.text_input("Your Message1:")
+save(input)
 
-all_msg = st.session_state.key
-all_msg.append(input)
-st.session_state['key'] = all_msg
-for msg in all_msg:
-	st.write(msg)
+messages = getAll() 
+st.write(messages)
