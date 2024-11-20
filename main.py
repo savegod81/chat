debded4325f158d4461fb78425adbe7ad2b6d04a
savegod81 <26392985@qq.com>
2024-11-20 17:29:11 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ if 'key' not in st.session_state:
     st.session_state['key'] = []
 
 st.title("Our Private Chatroom")
-input = st.text_input("Your Message2:")
+input = st.text_input("Your Message1:")
 
 all_msg = st.session_state.key
 all_msg.append(input)
-st.session_state['key'] = message
+st.session_state['key'] = all_msg
 for msg in all_msg:
 	st.write(msg)
