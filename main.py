852b2bcfa738014fe525e4279d15f3c0a72532f0
savegod81 <26392985@qq.com>
2024-11-20 17:09:37 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import streamlit as st
 
+messages = []
+
+@st.cache_data
+def save(msg):
+	messages.append(msg)
+	return messages
+
 st.title("Our Private Chatroom")
 input = st.text_input("Your Message:")
 
-st.write(input)
+all_msg = save(input)
+for msg in all_msg:
+	st.write(msg)
